69d6d97839dfd79eb59cf5fdecd17c4c237ba6dc
Mau <mauvepinktree@gmail.com>
2023-09-22 15:41:43 -0300

    Implemented player movement with keyboard keys

--- Algebra_TP1/src/game.cpp
+++ Algebra_TP1/src/game.cpp
@@ -8,30 +8,57 @@
 using namespace std;
 
 namespace Game {
+	struct Player {
+		Vector2 position;
+		float speed;
+	};
+
+	Player player;
 
 	static void draw();
+	static void handleControls();
 
 	void draw() {
 		BeginDrawing();
 			ClearBackground({ 0, 10, 0 });
-			DrawText("[Q] para entrar en modo editor.", 10, 10, 20, RAYWHITE);
-			DrawText("[ESC] para cerrar el programa.", 10, 40, 20, RAYWHITE);
+			DrawText("[WASD] para controlar al jugador.", 10, 10, 20, RAYWHITE);
+			DrawText("[Q] para entrar en modo editor.", 10, 40, 20, RAYWHITE);
+			DrawText("[ESC] para cerrar el programa.", 10, 70, 20, RAYWHITE);
 			Level::draw();
 
-			Level::checkCollisions(GetMousePosition()) ? DrawCircleV(GetMousePosition(), 20, GREEN) : DrawCircleV(GetMousePosition(), 20, RED);
+			Level::checkCollisions(player.position) ? DrawCircleV(player.position, 20, GREEN) : DrawCircleV(player.position, 20, RED);
 		EndDrawing();
+	}
+
+	void handleControls() {
+		if (IsKeyPressed(KEY_Q)) {
+			Program::setState(Program::ProgramState::DRAW);
+		}
+		// Player Controls
+		if (IsKeyDown(KEY_W)) {
+			player.position.y -= GetFrameTime() * player.speed;
+		}
+		if (IsKeyDown(KEY_A)) {
+			player.position.x -= GetFrameTime() * player.speed;
+		}
+		if (IsKeyDown(KEY_S)) {
+			player.position.y += GetFrameTime() * player.speed;
+		}
+		if (IsKeyDown(KEY_D)) {
+			player.position.x += GetFrameTime() * player.speed;
+		}
 	}
 
 	// Public
 	void update() {
-		if (IsKeyPressed(KEY_Q)) {
-			Program::setState(Program::ProgramState::DRAW);
-		}
+		handleControls();
 
 		draw();
 	}
 
 	void init() {
 		DisableCursor();
+		player.position = { (GetScreenWidth() * .5f), (GetScreenHeight() * .5f) };
+		player.speed = 250.0f;
 	}
 }
