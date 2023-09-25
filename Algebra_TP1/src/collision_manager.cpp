39958695b33a0e8d8a27e8512170a72fda5e9e94
Mau <mauvepinktree@gmail.com>
2023-09-24 22:13:34 -0300

    Removed comments

--- Algebra_TP1/src/collision_manager.cpp
+++ Algebra_TP1/src/collision_manager.cpp
@@ -16,21 +16,6 @@ namespace Collision {
             Vector2 vecCurrent = poly[current];    // El vector actual
             Vector2 vecNext = poly[next];       // El vector siguiente
 
-            /* 
-            Para determinar la colisión usamos esta formula, la cual es una implementación del Teorema de la Curva de Jordan.
-            Lo que hace esta función es verificar si nuestro punto (En nuestro caso "point") se encuentra dentro de un poligono.
-            (En este caso tenemos una forma compuesta por varios poligonos, los cuales recorremos con el for y comprobamos individualmente
-            del primero al ultimo al igual de como se dibujaron.)
-            
-            Esta condicion se divide en dos partes (Separadas por el &&)
-            La primera parte verifica que el punto se encuentre verticalmente entre los dos puntos del poligono actual (vecCurrent y vecNext).
-            La segunda parte verifica que el punto se encuentre horizontalmente dentro de la forma;
-            Esto lo logra usando el Teorema de la Curva de Jordan, esta verifica si el punto (point) cruza verticalmente por linea vertical que pasa por los dos
-            puntos del poligono actual (vecCurrent y vecNext). En este caso, usando el operador "!" podemos invertir esta variable de forma de que la colisión sea
-            verdadera unicamente si esta condición da verdadera un numero inpar de veces.
-
-            Cabe destacar que El teorema de la Curva de Jordan funciona unicamente con formas simples en 2 dimensiones.
-            */ 
             if (((vecCurrent.y >= point.y && vecNext.y < point.y) || (vecCurrent.y < point.y && vecNext.y >= point.y)) &&
                 (point.x < (vecNext.x - vecCurrent.x) * (point.y - vecCurrent.y) / (vecNext.y - vecCurrent.y) + vecCurrent.x)) {
                 collision = !collision;
