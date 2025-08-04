fec82ce235d001618f9ab703f116b0558c059f4b
Mau <mauvepinktree@gmail.com>
2025-08-04 02:18:20 -0300

    update extras

--- Algebra_TP1/src/collision_manager.cpp
+++ Algebra_TP1/src/collision_manager.cpp
@@ -3,8 +3,6 @@
 namespace Collision {
     // https://en.wikipedia.org/wiki/Point_in_polygon
 
-    int IsLeft(Vector2 P0, Vector2 P1, Vector2 P2);
-
     bool PointInPolygon(Vector2 point, vector<Vector2> poly) {
         bool collision = false;
         // Recorremos cada uno de los vertices + el que le siga.
@@ -30,42 +28,5 @@ namespace Collision {
             */
         }
         return collision;
-    }
-
-    int IsLeft(Vector2 P0, Vector2 P1, Vector2 P2) { // Producto cruzado.
-        return static_cast<int>((P1.x - P0.x) * (P2.y - P0.y) -
-                                (P2.x - P0.x) * (P1.y - P0.y));
-    } // > 0 izquierda. < 0 derecha. = 0 alineado.
-
-    bool PointInPolygonWinding(Vector2 point, const vector<Vector2>& poly) {
-        int wNum = 0;
-        int size = poly.size();
-
-        for (int i = 0; i < size; i++) {
-            Vector2 vecCurrent = poly[i];
-            Vector2 vecNext = poly[(i + 1) % size];
-
-            if (vecCurrent.y <= point.y) {
-                if (vecNext.y > point.y) {
-                    if (IsLeft(vecCurrent, vecNext, point) > 0)
-                        ++wNum;
-                }
-            } else {
-                if (vecNext.y <= point.y) {
-                    if (IsLeft(vecCurrent, vecNext, point) < 0)
-                        --wNum;
-                }
-            }
-            /*
-            Recorremos todos los vectores de el poligono.
-            Si estamos bajando y al mismo tiempo cruzando horizontalmente al punto
-            y ademas el punto se encuentra del lado izquierdo sumamos 1 punto.
-            Si estamos subiendo y al mismo tiempo cruzando horizontalmente al pundo
-            y ademas el punto se encuentra del lado derecho restamos 1 punto.
-            Si el resultado es 0 entonces nos encontramos dentro.
-            */
-        }
-
-        return wNum != 0;
     }
 }
